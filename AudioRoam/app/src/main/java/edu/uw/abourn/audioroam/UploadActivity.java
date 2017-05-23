dbd1f5c74368ed42d1fa0e074fec06adc35018dc
John Laws Harrison <johnlawsharrison@gmail.com>
2017-05-22 22:14:25 -0700

    Add basic upload fab

--- AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/UploadActivity.java
+++ AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/UploadActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class UploadActivity extends AppCompatActivity {
 
+    private static final String TAG = "UploadActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
