a0f460a8ad3c86030481b1aa68c1d76cc14dbac7
Adam Bourn <abourn@uw.edu>
2017-05-23 09:57:35 -0700

    configure firebase in android studio, add boiler plate/skeleton for signing in

--- AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/LoginActivity.java
+++ AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/LoginActivity.java
@@ -1,10 +1,23 @@
 package edu.uw.abourn.audioroam;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailInput;
+    private EditText passwordInput;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +25,63 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in, should sent intent to show main activity
+                } else {
+                    // User is signed out;
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void signIn(View v) {
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        validateLogin(email, password);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                        } else {
+                            // boi your password or username is wrong
+                        }
+                    }
+                });
+    }
+
+    public void validateLogin(String email, String password) {
+        // check to make sure that email and password aren't blank.
+        // show snackbar to user for feedback
+    }
+
+    public void forgotPasswordCallback(View v) {
+
+    }
+
+    public void signUpCallback(View v) {
+        // send intent to the SignUp Activity
     }
 }
