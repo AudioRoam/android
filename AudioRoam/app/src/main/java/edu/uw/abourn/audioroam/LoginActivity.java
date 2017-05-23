d58f74dfdafabbe970bbffb20aaf9756c0236c89
Adam Bourn <abourn@uw.edu>
2017-05-23 10:41:31 -0700

    snackbar when email/password fields blank, change firebase-auth to 10.2.6, update google repository

--- AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/LoginActivity.java
+++ AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/LoginActivity.java
@@ -1,9 +1,12 @@
 package edu.uw.abourn.audioroam;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,6 +15,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import static android.R.attr.password;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText emailInput;
@@ -58,23 +63,32 @@ public class LoginActivity extends AppCompatActivity {
     public void signIn(View v) {
         String email = emailInput.getText().toString();
         String password = passwordInput.getText().toString();
-        validateLogin(email, password);
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-
-                        } else {
-                            // boi your password or username is wrong
+        boolean valid = validateLogin(v, email, password);
+        if (valid) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // send dat intent
+                            } else {
+                                // boi your password or username is wrong
+                            }
                         }
-                    }
-                });
+                    });
+        }
     }
 
-    public void validateLogin(String email, String password) {
+    public boolean validateLogin(View v, String email, String password) {
         // check to make sure that email and password aren't blank.
         // show snackbar to user for feedback
+        if (email.isEmpty()) {
+            Snackbar.make(v, "Email cannot be blank!", Snackbar.LENGTH_SHORT).show();
+        }else if (password.isEmpty()) {
+            Snackbar.make(v, "Password cannot be blank!", Snackbar.LENGTH_SHORT).show();
+        }
+
+        return !email.isEmpty() && !password.isEmpty();
     }
 
     public void forgotPasswordCallback(View v) {
