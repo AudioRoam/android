c8b488e4168886e764fba4dc767af35f38bdcebe
Andrew Joung <andrew.joung@gmail.com>
2017-05-25 08:46:51 -0700

    Add working google map

--- AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/MapActivity.java
+++ AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/MapActivity.java
@@ -1,8 +1,17 @@
 package edu.uw.abourn.audioroam;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,6 +22,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GoogleApiClient myGoogleApiClient;
+
+    private static final int LOC_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +34,80 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if(myGoogleApiClient == null) {
+            myGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this) //what objects can respond to callbacks
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API) //which api I want client to connect to
+                    .build();
+        }
     }
 
+    //Calls to connect to the Google API client when the application is started
+    @Override
+    protected void onStart() {
+        myGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    //Disconnects from the Google API client when the application is stopped
+    //Checks to see if the application has permission to save a file
+    @Override
+    protected void onStop() {
+        myGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    //Once we are connect to the Google API
+    //Creates a new location request to get location of the user and to update location
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            // if permission granted, generate a listener for location change/send location request
+            mMap.setMyLocationEnabled(true);
+            LocationServices.FusedLocationApi.requestLocationUpdates(myGoogleApiClient, request, this);
+        } else {
+            // request the permission if not there..
+            // get permission
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOC_REQUEST_CODE);
+        }
+    }
+
+    //Checks the result of our permission requests
+    //Checks the location permission results and file saving request
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //if location permission granted
+        //call onConnected method and do those tasks
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            onConnected(null);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    //Callback that listens for a change in the user location
+    //Centers the camera based on the user's location
+    @Override
+    public void onLocationChanged(Location location) {
+        //if location has changed
+        if(location != null) {
+            LatLng initialLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(initialLocation));
+        }
     }
 }
