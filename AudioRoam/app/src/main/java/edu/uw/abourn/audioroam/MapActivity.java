183521fc27e4280dece0931b868f6817e9a03d25
Andrew Joung <andrew.joung@gmail.com>
2017-05-31 12:58:28 -0700

    no edit

--- AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/MapActivity.java
+++ AudioRoam/app/src/main/java/edu/uw/abourn/audioroam/MapActivity.java
@@ -93,7 +93,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
             }
         });
 
-
+        //TODO: Read from firebase, place existing tracks as markers onto the map 
 
     }
 
